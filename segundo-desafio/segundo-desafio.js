1ac4f77859209d0f12c429805e2ccbbe60444562
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-04-04 09:10:20 -0300

    segundo desafio com funções

--- segundo-desafio/segundo-desafio.js
+++ segundo-desafio/segundo-desafio.js
@@ -1,14 +1,55 @@
+// variavel para chamar e guardar os dados da função em um array
+let saldoVitorias = gerarSaldoDeVitorias()
 
-// variaveis para guardar as vitorias e derrotas do Heroi 
 
-// (Utilizei uma formula para declarar o valores aleatorios entre 50 e 250)
-let numVictories = Math.floor(Math.random() * (250 - 50) + 50)
-// (Utilizei uma formula para declarar o valores aleatorios entre 0 e 50)
-let numDefeats = Math.floor(Math.random() * (50 - 0) + 0)
+// condicional
+// Switch Case que ja chama a função caso seja verdadeiro
 
-let saldo = numVictories - numDefeats
+switch (true){
+    case saldoVitorias[2] < 10:
+        exibeResultado(saldoVitorias, "Ferro")
+    break
 
-console.log(`Total de vitórias: ${numVictories}`)
-console.log(`Total de derrotas: ${numDefeats}`)
-console.log(`Pontuação: ${saldo}`)
+    case saldoVitorias[2] >= 11 && saldoVitorias[2] < 20:
+        exibeResultado(saldoVitorias, "Bronze")
+    break
+    
+    case saldoVitorias[2] >= 21 && saldoVitorias[2] < 5:
+        exibeResultado(saldoVitorias, "Prata")
+    break
 
+    case saldoVitorias[2] >= 51 && saldoVitorias[2] < 80:
+        exibeResultado(saldoVitorias, "Ouro")
+    break
+
+    case saldoVitorias[2] >= 81 && saldoVitorias[2] < 90:
+        exibeResultado(saldoVitorias, "Diamante")
+    break
+
+    case saldoVitorias[2] >= 91 && saldoVitorias[2] < 100:
+        exibeResultado(saldoVitorias, "Lendário")
+    break
+
+    case saldoVitorias[2] >= 101:
+        exibeResultado(saldoVitorias, "Imortal")
+    break
+}
+
+// função que gera a pontuação do heróis
+function gerarSaldoDeVitorias() {
+    // (Utilizei uma formula para declarar o valores aleatorios entre 50 e 150)
+    let numVictories = Math.floor(Math.random() * (150 - 50) + 50)
+    // (Utilizei uma formula para declarar o valores aleatorios entre 0 e 50)
+    let numDefeats = Math.floor(Math.random() * (50 - 0) + 0)
+    // o saldo é igual a numero de vitorias menos o de derrotas
+    let saldo = numVictories - numDefeats
+    // crio um array com os dados
+    let dadosDoHeroi = [numVictories, numDefeats, saldo]
+    //retorno o array
+    return dadosDoHeroi
+}
+
+// função que exibe o resultato
+function exibeResultado(saldoVitorias, nivel) {
+    console.log(`O Herói tem de saldo de ${saldoVitorias[2]} vitórias está no nível de ${nivel}`)
+}
